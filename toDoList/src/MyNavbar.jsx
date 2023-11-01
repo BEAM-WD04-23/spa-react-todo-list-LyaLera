b8a14a3afab3597c17d395155309132bf8ba6a84
Valeriia Lyakisheva <vorik07@icloud.com>
2023-11-01 15:05:39 +0100

    Worked on style a little

--- toDoList/src/MyNavbar.jsx
+++ toDoList/src/MyNavbar.jsx
@@ -2,16 +2,21 @@ import { Link } from "react-router-dom"
 
 export default function MyNavbar() {
   return (
+    <>
     <nav>
-      <div className="home">
-        <Link to="/">Home</Link>
-      </div>
-      <div className="todolist">
-        <Link to="/ToDoApp">My ToDo List</Link>
-      </div>
-      <div className="meal">
-        <Link to="/Meal">A Meal to cook today</Link>
-      </div>
+        <div className="nav-div">
+          <Link className="home link" to="/">Home</Link>
+        </div>
+        <div className="nav-div">
+          <Link className="link" to="/ToDoApp">My ToDo List</Link>
+        </div>
+        <div className="nav-div">
+          <Link className="link" to="/Meal">A Meal to cook today</Link>
+        </div>
     </nav>
+    <h2>Do not know what to do?</h2>
+    <img width={"300px"} src="https://cdn.pixabay.com/photo/2019/01/17/13/55/cat-3937859_1280.jpg" alt="bored cat" />
+    <p>Check your ToDo list or cook a random Meal:)</p>
+    </>
   );
 }
--- toDoList/src/ToDoApp.jsx
+++ toDoList/src/ToDoApp.jsx
@@ -79,7 +79,7 @@ export default function ToDoApp() {
         deleteTask={deleteTaskFromList}
         changeTask={changeTask}
       />
-      <button><Link to='/'>Back to Home Page</Link></button>
+      <button><Link className="link" to='/'>Back to Home Page</Link></button>
     </>
   );
 }

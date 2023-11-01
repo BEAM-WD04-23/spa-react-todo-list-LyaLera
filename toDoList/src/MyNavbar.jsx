547cab3318820e4dfa9e081e2a7dfb979ad728d6
Valeriia Lyakisheva <vorik07@icloud.com>
2023-11-01 20:30:39 +0100

    Added Home component, changed Link to NavLink with styling feature

--- toDoList/src/MyNavbar.jsx
+++ toDoList/src/MyNavbar.jsx
@@ -1,22 +1,17 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 export default function MyNavbar() {
   return (
-    <>
     <nav>
         <div className="nav-div">
-          <Link className="home link" to="/">Home</Link>
+          <NavLink className="home link" to="/">Home</NavLink>
         </div>
         <div className="nav-div">
-          <Link className="link" to="/ToDoApp">My ToDo List</Link>
+          <NavLink className="link" to="/ToDoApp">My ToDo List</NavLink>
         </div>
         <div className="nav-div">
-          <Link className="link" to="/Meal">A Meal to cook today</Link>
+          <NavLink className="link" to="/Meal">A Meal to cook today</NavLink>
         </div>
     </nav>
-    <h2>Do not know what to do?</h2>
-    <img width={"300px"} src="https://cdn.pixabay.com/photo/2019/01/17/13/55/cat-3937859_1280.jpg" alt="bored cat" />
-    <p>Check your ToDo list or cook a random Meal:)</p>
-    </>
   );
 }
